a41b6de1ab300a9297ac4454a66670e0a517c426
zltmmikls10 <zltmmikls10@gmail.com>
2019-11-13 20:25:01 +0100

    Lófaszkodás, majd jön a többi

---
+++ core/src/hu/cehessteg/vizeromu/Kiomlonyilas.java
@@ -0,0 +1,17 @@
+package hu.cehessteg.vizeromu;
+
+public class Kiomlonyilas {
+    public boolean isOpen;
+
+    public Kiomlonyilas() {
+
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+}
--- core/src/hu/cehessteg/vizeromu/Matek.java
+++ core/src/hu/cehessteg/vizeromu/Matek.java
@@ -19,5 +19,41 @@ public class Matek {
     //árvízveszély a városra nézve
     //szárazság calc
 
+    float vizmennyiseg = 1000000; // 1misivel kezdünk;
+    int minviz = 1000; //gameover ha kevesebb
+    int maxviz = 1100000; //gameover ha nagyobb
+    int beviz = 100; //befolyó víz
+    int kiviz = 40; //azért ne legyen pont osztható a bevizzel(annyira azért ne legyen könnyű)
+    int ora = 8;
+    int perc = 0;
+    boolean eso = false;
+    boolean volteso = false;
+    Kiomlonyilas[] nyilasok = new Kiomlonyilas[5];
+    int openek = 0; // mennyi van nyitva
 
+    public Matek() {
+        for (int i = 0; i < 5; i++) {
+            Kiomlonyilas res = new Kiomlonyilas();
+            nyilasok[i] =  res;
+        }
+    }
+
+    void opencounter(){
+        int a = 0;
+        for (Kiomlonyilas b : nyilasok) {
+            if(b.isOpen) a++;
+        }
+        openek=a;
+    }
+
+    public void main(String[] args) {
+        if (eso) {
+            vizmennyiseg += 50;
+            if(!volteso){} //animation trigger
+            else{volteso = true;}
+        }else{}
+
+        vizmennyiseg += beviz;
+        vizmennyiseg -= kiviz * openek;
+    }
 }
