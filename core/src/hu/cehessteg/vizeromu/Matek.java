ab797ee117b6f35316dc5966a77e4bc34c928287
marton552 <marton552@gmail.com>
2019-11-13 15:38:33 +0100

    Csak ötletek kommentel

---
+++ core/src/hu/cehessteg/vizeromu/Matek.java
@@ -0,0 +1,23 @@
+package hu.cehessteg.vizeromu;
+
+public class Matek {
+    //if esik akkor percenként 50% esély rá hogy eláll következő checknél, ha nem akkor 1-2% esély hogy elkezd esni. Eső = + bemenő víz!
+    //kell időt mérni
+    //kell pénz kalkulálni (valahogy)
+    //szélsőértékszámtás (min és max mert game over)
+    //Vízzel való számítások(bemenő, kimenő, random események)
+    //gátak be ki kapcsolása és ezekhez lekérdezés
+    //
+    //Bónusz ötletek miután már a minimum megvan:
+    //Atomreaktor a háttérbe és hozzá easter egg
+    //túristáka gátok
+    //több random esemény
+    //víznyomás kalkuláció (ez-to-hard math)
+    //karbantartás(lezárás, javítási költség)
+    //power upok (nagyobb eséllyel eső pl vagy + víz időnként)
+    //nehézségi szintek
+    //árvízveszély a városra nézve
+    //szárazság calc
+
+
+}
