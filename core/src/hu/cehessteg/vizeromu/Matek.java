5855a97be1fb277aa294e07b10c4cb0ebc3da83f
zltmmikls10 <zltmmikls10@gmail.com>
2019-11-16 10:49:34 +0100

    A legfelső kommentek a készek, középső még fontos lenne, az alsók pedig opcionálisak

--- core/src/hu/cehessteg/vizeromu/Matek.java
+++ core/src/hu/cehessteg/vizeromu/Matek.java
@@ -1,10 +1,13 @@
 package hu.cehessteg.vizeromu;
 
+import java.util.Random;
+
 public class Matek {
-    //if esik akkor percenként 50% esély rá hogy eláll következő checknél, ha nem akkor 1-2% esély hogy elkezd esni. Eső = + bemenő víz!
-    //kell időt mérni
+    //if esik akkor percenként 50% esély rá hogy eláll következő checknél, ha nem akkor 1-2% esély hogy elkezd esni. Eső = + bemenő víz! készen
+    //kell időt mérni készen
+    //szélsőértékszámtás (min és max mert game over) készen, de lehet hogy finomítani kell
+    //
     //kell pénz kalkulálni (valahogy)
-    //szélsőértékszámtás (min és max mert game over)
     //Vízzel való számítások(bemenő, kimenő, random események)
     //gátak be ki kapcsolása és ezekhez lekérdezés
     //
@@ -26,34 +29,57 @@ public class Matek {
     int kiviz = 40; //azért ne legyen pont osztható a bevizzel(annyira azért ne legyen könnyű)
     int ora = 8;
     int perc = 0;
+    int sec = 0;
     boolean eso = false;
     boolean volteso = false;
     Kiomlonyilas[] nyilasok = new Kiomlonyilas[5];
     int openek = 0; // mennyi van nyitva
 
+    Random rnd = new Random();
+
     public Matek() {
         for (int i = 0; i < 5; i++) {
             Kiomlonyilas res = new Kiomlonyilas();
-            nyilasok[i] =  res;
+            nyilasok[i] = res;
         }
     }
 
-    void opencounter(){
+    void opencounter() {
         int a = 0;
         for (Kiomlonyilas b : nyilasok) {
-            if(b.isOpen) a++;
+            if (b.isOpen) a++;
         }
-        openek=a;
+        openek = a;
     }
 
     public void main(String[] args) {
         if (eso) {
             vizmennyiseg += 50;
-            if(!volteso){} //animation trigger
-            else{volteso = true;}
-        }else{}
-
+            if (!volteso) { volteso = true; } //animation trigger
+            else {
+                if (rnd.nextInt(2) == 1) { eso = false; }}} //eso elmegy 50% eséllyel.
+        else {
+            if (sec == 0 && rnd.nextInt(100) == 13) { eso = false; } //eso megered 1% eséllyel.
+        }
         vizmennyiseg += beviz;
         vizmennyiseg -= kiviz * openek;
+        //ido
+        sec++;
+        if (sec == 60) {
+            sec = 0;
+            perc++;
+        }
+        if (perc == 60) {
+            perc = 0;
+            ora++;
+        }
+        if (ora == 24) {
+            ora = 0;
+        }
+
+        //gameover trigger
+        if (vizmennyiseg <= minviz || vizmennyiseg >= maxviz) {}//gameover trigger
     }
+
 }
+
